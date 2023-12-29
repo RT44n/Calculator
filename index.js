6d14a78ee7622660518a376cccc005824bee962e
RT44n <147970845+RT44n@users.noreply.github.com.>
2023-12-29 19:05:57 +0530

    Modify operator functions

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const multiply = function (array) {
-  return array.reduce((product, current) => product * current);
+const multiply = function (a, b) {
+  return a * b;
 };
 
-const divide = function (array) {
-  return array.reduce((quotient, current) => quotient / current);
+const divide = function (a, b) {
+  return a / b;
 };
