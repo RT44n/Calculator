0f02d14b07421532e5d8f0595e11c878214a5389
RT44n <147970845+RT44n@users.noreply.github.com.>
2023-12-30 11:02:34 +0530

    Create const for calc op create func for full op
    1. Three const variables for each of the parts of a calculator operation. A variable for the first number, the operator, and the second number.
    2. A new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.

--- index.js
+++ index.js
@@ -13,3 +13,34 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+const getFirstNumber = () => {
+  let firstNumber = prompt("What's your first number?");
+
+  return firstNumber;
+};
+const getOperator = () => {
+  let operator = prompt("Whats's your operator?");
+
+  return operator;
+};
+const getSecondNumber = () => {
+  let secondNumber = prompt("What's your second number?");
+
+  return secondNumber;
+};
+const operate = () => {
+  let first = parseInt(getFirstNumber());
+  let operand = getOperator();
+  let second = parseInt(getSecondNumber());
+
+  console.log(first);
+  console.log(operand);
+  console.log(second);
+
+  if (operand == "+") return add(first, second);
+  else if (operand == "*") return multiply(first, second);
+  else if (operand == "-") return subtract(first, second);
+  else if (operand == "/") return divide(first, second);
+};
+console.log(operate());
